359c5e6de88d95ca1b84073656f5006d7ed48449
faarukbulut <faarukbulut@gmail.com>
2023-12-11 19:26:49 +0300

    Diğer API Controller'lar yazıldı

---
+++ SignalRApi/Controllers/ContactController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.ContactDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ContactController : ControllerBase
+	{
+		private readonly IContactService _contactService;
+		private readonly IMapper _mapper;
+
+		public ContactController(IContactService contactService, IMapper mapper)
+		{
+			_contactService = contactService;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public IActionResult ContactList()
+		{
+			var values = _mapper.Map<List<ResultContactDto>>(_contactService.TGetListAll());
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CreateContact(CreateContactDto createContactDto)
+		{
+			_contactService.TAdd(new Contact()
+			{
+				FooterDescription = createContactDto.FooterDescription,
+				Location = createContactDto.Location,
+				Mail = createContactDto.Mail,
+				Phone = createContactDto.Phone,
+			});
+			return Ok("Başarıyla eklendi.");
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteContact(int id)
+		{
+			var value = _contactService.TGetByID(id);
+			_contactService.TDelete(value);
+			return Ok("Başarıyla silindi.");
+		}
+
+		[HttpPut]
+		public IActionResult UpdateContact(UpdateContactDto updateContactDto)
+		{
+			_contactService.TUpdate(new Contact()
+			{
+				ContactID= updateContactDto.ContactID,
+				FooterDescription = updateContactDto.FooterDescription,
+				Phone= updateContactDto.Phone,
+				Mail= updateContactDto.Mail,
+				Location = updateContactDto.Location,
+			});
+			return Ok("Başarıyla güncellendi.");
+		}
+
+		[HttpGet("GetContact")]
+		public IActionResult GetContact(int id)
+		{
+			var value = _contactService.TGetByID(id);
+			return Ok(value);
+		}
+
+	}
+}
---
+++ SignalRApi/Controllers/TestimonialController.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.TestimonialDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestimonialController : ControllerBase
+	{
+		private readonly ITestimonialService _testimonialService;
+		private readonly IMapper _mapper;
+
+		public TestimonialController(ITestimonialService testimonialService, IMapper mapper)
+		{
+			_testimonialService = testimonialService;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public IActionResult TestimonialList()
+		{
+			var values = _mapper.Map<List<ResultTestimonialDto>>(_testimonialService.TGetListAll());
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CreateTestimonial(CreateTestimonialDto createTestimonialDto)
+		{
+			_testimonialService.TAdd(new Testimonial()
+			{
+				Title = createTestimonialDto.Title,
+				Comment = createTestimonialDto.Comment,
+				ImageUrl = createTestimonialDto.ImageUrl,
+				Name = createTestimonialDto.Name,
+				Status = true,
+			});
+			return Ok("Başarıyla eklendi.");
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteTestimonial(int id)
+		{
+			var value = _testimonialService.TGetByID(id);
+			_testimonialService.TDelete(value);
+			return Ok("Başarıyla silindi.");
+		}
+
+		[HttpPut]
+		public IActionResult UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+		{
+			_testimonialService.TUpdate(new Testimonial()
+			{
+				TestimonialID = updateTestimonialDto.TestimonialID,
+				Title = updateTestimonialDto.Title,
+				Comment = updateTestimonialDto.Comment,
+				ImageUrl = updateTestimonialDto.ImageUrl,
+				Name = updateTestimonialDto.Name,
+				Status = updateTestimonialDto.Status,
+			});
+			return Ok("Başarıyla güncellendi.");
+		}
+
+		[HttpGet("GetTestimonial")]
+		public IActionResult GetTestimonial(int id)
+		{
+			var value = _testimonialService.TGetByID(id);
+			return Ok(value);
+		}
+
+	}
+}
