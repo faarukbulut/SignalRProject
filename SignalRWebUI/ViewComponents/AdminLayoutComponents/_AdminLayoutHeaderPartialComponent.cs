6c3b325b75bc81fcbede2ed9b5ab0ba652a6fff2
faarukbulut <faarukbulut@gmail.com>
2023-12-11 20:39:56 +0300

    Admin Layout Eklendi.

---
+++ SignalRWebUI/ViewComponents/AdminLayoutComponents/_AdminLayoutHeaderPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.AdminLayoutComponents
+{
+	public class _AdminLayoutHeaderPartialComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ SignalRWebUI/ViewComponents/AdminLayoutComponents/_AdminLayoutSidebarPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.AdminLayoutComponents
+{
+	public class _AdminLayoutSidebarPartialComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
