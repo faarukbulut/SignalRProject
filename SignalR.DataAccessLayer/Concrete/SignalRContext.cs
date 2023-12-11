d585d0c6ddb14d273dea7a4a7b87fb387238edcd
faarukbulut <faarukbulut@gmail.com>
2023-12-11 16:45:17 +0300

    Bağlantı Sınıfının Yazılması

--- SignalR.DataAccessLayer/Concrete/SignalRContext.cs
+++ SignalR.DataAccessLayer/Concrete/SignalRContext.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalR.DataAccessLayer.Concrete
 {
-	public class SignalRContext
+	public class SignalRContext : DbContext
 	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=FARUK\\SQLEXPRESS;initial catalog=SignalRDb;integrated Security=true");
+		}
+
+		public DbSet<About> Abouts { get; set; }
+		public DbSet<Booking> Bookings { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<Discount> Discounts { get; set; }
+		public DbSet<Feature> Features { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<SocialMedia> SocialMedias { get; set; }
+		public DbSet<Testimonial> Testimonials { get; set; }
 	}
 }
