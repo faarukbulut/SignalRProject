f667210c07bf471d6683bd322ea119df502d005d
faarukbulut <faarukbulut@gmail.com>
2023-12-12 18:51:24 +0300

    Money Case İstatistiği

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -54,6 +54,10 @@ builder.Services.AddScoped<IOrderDal, EfOrderDal>();
 builder.Services.AddScoped<IOrderDetailService, OrderDetailManager>();
 builder.Services.AddScoped<IOrderDetailDal, EfOrderDetailDal>();
 
+builder.Services.AddScoped<IMoneyCaseService, MoneyCaseManager>();
+builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
+
+
 
 
 builder.Services.AddControllers();
