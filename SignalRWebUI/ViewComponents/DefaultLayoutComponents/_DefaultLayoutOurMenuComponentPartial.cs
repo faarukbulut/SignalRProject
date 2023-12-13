77885a7c203138df47cfddc6f58f4a317c2b25f8
faarukbulut <faarukbulut@gmail.com>
2023-12-13 18:02:26 +0300

    Anasayfa Menü Consume İşlemi

--- SignalRWebUI/ViewComponents/DefaultLayoutComponents/_DefaultLayoutOurMenuComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultLayoutComponents/_DefaultLayoutOurMenuComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ProductDto;
 
 namespace SignalRWebUI.ViewComponents.DefaultLayoutComponents
 {
     public class _DefaultLayoutOurMenuComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultLayoutOurMenuComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44330/api/Product/ProductListWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductWithCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
