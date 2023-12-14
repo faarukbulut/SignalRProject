5ee02dfcbfc93eceb1b6e7be8632a0eeafda84ab
faarukbulut <faarukbulut@gmail.com>
2023-12-14 22:33:42 +0300

    Masaların Boş ve Dolululuk Durumları

--- SignalRWebUI/Controllers/MenuTableController.cs
+++ SignalRWebUI/Controllers/MenuTableController.cs
@@ -93,5 +93,22 @@ namespace SignalRWebUI.Controllers
 
 			return View();
 		}
+
+
+		public async Task<IActionResult> TableListByStatus()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:44330/api/MenuTable");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultMenuTableDto>>(jsonData);
+				return View(values);
+			}
+
+			return View();
+		}
+
+
 	}
 }
