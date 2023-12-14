fd6bb91342746b073bb0e3c4a3e0e9244118ba76
faarukbulut <faarukbulut@gmail.com>
2023-12-14 13:20:20 +0300

    Basket sınıfının Mimariye Entegre Edilmesi

---
+++ SignalR.DataAccessLayer/EntityFramework/EfBasketDal.cs
@@ -0,0 +1,21 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfBasketDal : GenericRepository<Basket>, IBasketDal
+    {
+        public EfBasketDal(SignalRContext context) : base(context)
+        {
+        }
+
+        public List<Basket> GetBasketByTableNumber(int id)
+        {
+            using var context = new SignalRContext();
+            var values = context.Baskets.Where(x => x.MenuTableID == id).ToList();
+            return values;
+        }
+    }
+}
