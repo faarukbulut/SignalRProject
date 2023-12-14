70c43bd31aabfe4399cbc8f6d049bbfe2380f0ad
faarukbulut <faarukbulut@gmail.com>
2023-12-14 20:45:36 +0300

    Notification Sınıfı Api Alanı

--- SignalRApi/Controllers/NotificationController.cs
+++ SignalRApi/Controllers/NotificationController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -36,5 +38,47 @@ namespace SignalRApi.Controllers
 			return Ok(values);
 		}
 
+		[HttpPost]
+		public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+		{
+			_notificationService.TAdd(new Notification()
+			{
+				Description= createNotificationDto.Description,
+				Icon= createNotificationDto.Icon,
+				Date= createNotificationDto.Date,
+				Status = false,
+				Type= createNotificationDto.Type,
+			});
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteNotification(int id)
+		{
+			var value = _notificationService.TGetByID(id);
+			_notificationService.TDelete(value);
+			return Ok("Başarıyla silindi.");
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetNotification(int id)
+		{
+			var value = _notificationService.TGetByID(id);
+			return Ok(value);
+		}
+
+
+		[HttpPut]
+		public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+		{
+			_notificationService.TUpdate(new Notification()
+			{
+				NotificationID = updateNotificationDto.NotificationID
+				Description = updateNotificationDto.Description,
+				Icon = updateNotificationDto.Icon,
+				Date = updateNotificationDto.Date,
+				Status = updateNotificationDto.Status,
+				Type = updateNotificationDto.Type,
+			});
+		}
 	}
 }
