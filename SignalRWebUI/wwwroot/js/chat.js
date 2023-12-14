f4c01048e6213f080b49109c46fa1bd93628995c
faarukbulut <faarukbulut@gmail.com>
2023-12-14 23:32:45 +0300

    chat hata düzeltmesi

--- SignalRWebUI/wwwroot/js/chat.js
+++ SignalRWebUI/wwwroot/js/chat.js
@@ -2,21 +2,17 @@
 document.getElementById("sendbutton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
-    var currenttime = new Date();
-    var currentHour = currenttime.getHours();
-    var currentMinute = currenttime.getMinutes();
+    var currentTime = new Date();
+    var currentHour = currentTime.getHours();
+    var currentMinute = currentTime.getMinutes();
 
     var li = document.createElement("li");
     var span = document.createElement("span");
-
-    span.style.fontWeight = bold;
+    span.style.fontWeight = "bold";
     span.textContent = user;
-
     li.appendChild(span);
     li.innerHTML += ` :${message} - ${currentHour}:${currentMinute}`;
-
     document.getElementById("messagelist").appendChild(li);
-
 });
 
 connection.start().then(function () {
@@ -28,10 +24,8 @@ connection.start().then(function () {
 document.getElementById("sendbutton").addEventListener("click", function (event) {
     var user = document.getElementById("userinput").value;
     var message = document.getElementById("messageinput").value;
-
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
-
     event.preventDefault();
 });
