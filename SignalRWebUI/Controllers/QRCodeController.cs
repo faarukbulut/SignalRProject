255ab16476e0d2bc8c609237ee80e8f160760068
faarukbulut <faarukbulut@gmail.com>
2023-12-15 22:25:44 +0300

    QR Code View Tarafı

--- SignalRWebUI/Controllers/QRCodeController.cs
+++ SignalRWebUI/Controllers/QRCodeController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QRCoder;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace SignalRWebUI.Controllers
 {
 	public class QRCodeController : Controller
 	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
 		public IActionResult Index(string value)
 		{
 			using (MemoryStream memoryStream = new MemoryStream())
@@ -15,7 +23,8 @@ namespace SignalRWebUI.Controllers
 
 				using (Bitmap image = squareCode.GetGraphic(10))
 				{
-
+					image.Save(memoryStream, ImageFormat.Png);
+					ViewBag.QrCodeImage = "data:image/png;base64,"+Convert.ToBase64String(memoryStream.ToArray());
 				}
 			}
 
