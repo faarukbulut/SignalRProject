51d9118e1f879208e01b6568871ac95ea521b572
faarukbulut <faarukbulut@gmail.com>
2023-12-15 23:15:25 +0300

    Bilgileri güncellleme + qrcode.dll

--- SignalRWebUI/Controllers/SettingController.cs
+++ SignalRWebUI/Controllers/SettingController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using SignalR.EntityLayer.Entities;
 using SignalRWebUI.Dtos.IdentityDto;
 
@@ -26,6 +27,25 @@ namespace SignalRWebUI.Controllers
 			userEditDto.Username = value.UserName;
 
 			return View(userEditDto);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(UserEditDto userEditDto)
+		{
+			if(userEditDto.Password == userEditDto.Password2)
+			{
+				var user = await _userManager.FindByNameAsync(User.Identity.Name);
+				user.Name = userEditDto.Name;
+				user.Surname = userEditDto.Surname;
+				user.Email = userEditDto.Mail;
+				user.UserName = userEditDto.Username;
+				user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditDto.Password);
+
+				var result = await _userManager.UpdateAsync(user);
+				return RedirectToAction("Index");
+			}
+
+			return View();
 		}
 	}
 }
